3039141ffc2d35901f813b196c1554d22f2ba112
sumit <sgupta40@sapient.com>
2016-04-25 12:07:50 +0530

    FTO-305 - Malay #comment Billing Countries configured for CFTO #Reviewer Sumit * New CFTO site need to reuse the .com billing address * Created a fallback in code to fetch the billing countries mapped to * Parent store    in case countries not availble in child store    * Fetching the mapping of child to Parent store from businessconfig

--- src/com/helloworld/Test.java
+++ src/com/helloworld/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubsasadscf
+		// TODO Auto-generated method stubsasadscf  ssa
 		//Sumit this sidedsdsdsdsds
 		//sasadsaasdsadsa
 		System.out.println("Hello world Release branch");
